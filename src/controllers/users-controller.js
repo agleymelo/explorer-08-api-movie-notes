fba34dc10f5788b47f07bf3c55ca3619f7fae464
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-08 16:37:23 -0300

    Add bcrypt for hashed password user

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,3 +1,5 @@
+const { hash } = require("bcryptjs")
+
 const knex = require("../database/knex")
 const AppError = require("../utils/app-error")
 
@@ -12,10 +14,12 @@ class UsersController {
       throw new AppError("User already exists")
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await knex("users").insert({
       name,
       email,
-      password
+      password: hashedPassword
     })
 
     return response.status(201).json()
