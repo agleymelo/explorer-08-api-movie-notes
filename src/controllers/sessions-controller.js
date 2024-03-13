9ba79bd66439c344144f980e0df83d8eb3bcc597
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-13 14:08:34 -0300

    .gitignore is now working

--- src/controllers/sessions-controller.js
+++ src/controllers/sessions-controller.js
@@ -1,25 +1,22 @@
-const { compare } = require("bcryptjs")
+const { compare } = require('bcryptjs')
 
-const knex = require("../database/knex")
+const knex = require('../database/knex')
 const AppError = require('../utils/app-error')
 class SessionsController {
-
   async create(request, response) {
     const { email, password } = request.body
 
-    const user = await knex("users").where({ email }).first()
+    const user = await knex('users').where({ email }).first()
 
     if (!user) {
-      throw new AppError("E-mail or password incorrent", 401)
+      throw new AppError('E-mail or password incorrent', 401)
     }
 
     const passwordMatched = await compare(password, user.password)
 
     if (!passwordMatched) {
-      throw new AppError("E-mail or password incorrent", 401)
+      throw new AppError('E-mail or password incorrent', 401)
     }
-
-
 
     return response.status(200).json({ user })
   }
