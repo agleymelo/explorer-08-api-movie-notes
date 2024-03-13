252ddfb5bda0e7875c10baf1da01ead66ae3598e
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-13 15:31:10 -0300

    Middleware de autenticação

---
+++ src/middleware/ensureAuthenticated.js
@@ -0,0 +1,28 @@
+const { verify } = require("jsonwebtoken")
+
+const AppError = require("../utils/app-error")
+const authConfig = require("../config/auth")
+
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError("JWT token not provided", 401)
+  }
+
+  const [_, token] = authHeader.split(" ")
+
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret)
+
+    request.user = {
+      id: Number(user_id),
+    }
+
+    return next()
+  } catch (error) {
+    throw new AppError("JWT Token invalid", 401)
+  }
+}
+
+module.exports = ensureAuthenticated
