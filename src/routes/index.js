c3c70a5bd5655d1b89dfbb2c917103adc29b27cd
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-13 14:01:38 -0300

    Rotas de autenticação

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const { Router } = require("express")
+
 const usersRoutes = require("./user.router")
 const movieRoutes = require("./movie-notes.router")
+const sessionRoutes = require("./session.router")
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/movies", movieRoutes)
+routes.use("/sessions", sessionRoutes)
 
 module.exports = routes
