b4273d746461a47a13244a31a44311073598f3ae
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-14 19:44:55 -0300

    route /file for static files

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require("express-async-errors")
 const express = require("express")
 const cors = require("cors")
 
+const uploadConfig = require("./config/upload")
+
 const AppError = require("./utils/app-error")
 const routes = require("./routes")
 
@@ -10,6 +12,7 @@ const app = express()
 
 app.use(cors())
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(express.json())
 app.use(routes)
 
